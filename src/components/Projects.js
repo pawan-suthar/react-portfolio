04aee69efe502ec1d8bc19085602d1369709effc
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-04-13 14:07:10 +0530

    skills section added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,10 @@
 import React from "react";
 import pblog from "../assets/portfolio/pblog.png";
-import installNode from "../assets/portfolio/installNode.jpg";
+// import installNode from "../assets/portfolio/installNode.jpg";
 import navbar from "../assets/portfolio/navbar.jpg";
 import jobmatchup from "../assets/portfolio/jobmatchup.png";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+
 import { FaGithubAlt } from "react-icons/fa";
 import { TiLink } from "react-icons/ti";
 
@@ -46,66 +46,61 @@ const Portfolio = () => {
       hashtag: "#python #tech",
       desc: "With this application, the admin can easily post blogs and manage them according to categories they create. Users can access all the blogs and filter them based on categories, ensuring they can easily find the content they're interested in.",
     },
-    {
-      id: 5,
-      src: installNode,
-      title: "famework",
-      demo: "link",
-      github: "link",
-      hashtag: "#python #tech",
-      desc: "With this application, the admin can easily post blogs and manage them according to categories they create. Users can access all the blogs and filter them based on categories, ensuring they can easily find the content they're interested in.",
-    },
-    {
-      id: 6,
-      src: reactWeather,
-      title: "famework",
-      demo: "link",
-      github: "link",
-      hashtag: "#python #tech",
-      desc: "With this application, the admin can easily post blogs and manage them according to categories they create. Users can access all the blogs and filter them based on categories, ensuring they can easily find the content they're interested in.",
-    },
   ];
 
   return (
     <div
-      name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full  text-white h-screen md:h-screen"
+      name="projects"
+      className="bg-gradient-to-b from-black via-gra to-gray-800 w-full text-white h-screen md:h-screen"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center justify-items-center w-full h-full">
-        <div className="pb-4 pt-96 ">
-          <p className="text-4xl font-bold  border-b-4 border-gray-500 pb-2">
-            Projects
-          </p>
-        </div>
+      <div className="container flex justify-center items-center">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center justify-items-center w-full">
+          <div className="pb-4 pt-24 ">
+            <p className="text-4xl font-bold border-b-4 border-gray-500 pb-2">
+              Projects
+            </p>
+          </div>
 
-        <div class="p-5 grid sm:grid-cols-2 md:grid-cols-3 gap-8  flex-col justify-center">
-          {portfolios.map(({ id, src, title, demo, github, hashtag, desc }) => (
-            <div key={id} class="  max-w-sm rounded overflow-hidden shadow-lg">
-              <img class="w-full" src={src} alt="Mountain" />
-              <div class="px-6 py-4">
-                <div class="font-bold text-lg mb-1   text-white">{title}</div>
-                <p class="text-gray-200 text-base ">{desc}</p>
-              </div>
-              <div class="">
-                <div className="bg-transparent flex justify-start mt-2 mb-2 ml-3 cursor-pointer">
-                  <a href={demo} target="_blank" rel="noopener noreferrer">
-                    <TiLink size={29} className="text-white text-xl mr-5" />
-                    <p className="text-sm font-light">Demo</p>
-                  </a>
-                  <a href={github} target="_blank" rel="noopener noreferrer">
-                    <FaGithubAlt
-                      size={29}
-                      className="text-gray-200 text-xl mr-5"
-                    />
-                    <p className="text-sm font-light">Github</p>
-                  </a>
+          <div className="p-5 grid sm:grid-cols-2 md:grid-cols-4 gap-8 flex-row justify-start">
+            {portfolios.map(
+              ({ id, src, title, demo, github, hashtag, desc }) => (
+                <div
+                  key={id}
+                  className="max-w-sm rounded overflow-hidden shadow-lg"
+                >
+                  <img className="w-full" src={src} alt="Mountain" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-mb mb-1 text-white">
+                      {title}
+                    </div>
+                    <p className="text-gray-200 text-sm">{desc}</p>
+                  </div>
+                  <div className="">
+                    <div className="bg-transparent flex justify-start mt-2 mb-2 ml-3 cursor-pointer">
+                      <a href={demo} target="_blank" rel="noopener noreferrer">
+                        <TiLink size={29} className="text-white text-xl mr-5" />
+                        <p className="text-sm font-light">Demo</p>
+                      </a>
+                      <a
+                        href={github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaGithubAlt
+                          size={29}
+                          className="text-gray-200 text-xl mr-5"
+                        />
+                        <p className="text-sm font-light">Github</p>
+                      </a>
+                    </div>
+                    <span className="inline-block px-3 py-1 text-sm font-semibold text-green-600 mr-2 mb">
+                      {hashtag}
+                    </span>
+                  </div>
                 </div>
-                <span class="inline-block  px-3 py-1 text-sm font-semibold text-green-600 mr-2 mb">
-                  {hashtag}
-                </span>
-              </div>
-            </div>
-          ))}
+              )
+            )}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Skills.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div
+      name="skills"
+      className="bg-gradient-to-b from-gray-800 via-black to-black w-full text-white h-screen md:h-screen"
+    >
+      <div className="container flex flex-row justify-center items-center">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center justify-items-center w-full">
+          <div className="pb-4 pt-24 ">
+            <p className="text-4xl font-bold border-b-4 border-gray-500 pb-2">
+              Skills
+            </p>
+          </div>
+          <div className="flex flex-wrap justify-evenly">
+            <img
+              className="my-2 mr-5 h-28 w-28 rounded-full max-w-xs "
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png"
+              alt="python "
+            />
+            <img
+              className="my-2 mr-5 h-28 w-28 rounded-full max-w-xs "
+              src="https://play-lh.googleusercontent.com/RslBy1o2NEBYUdRjQtUqLbN-ZM2hpks1mHPMiHMrpAuLqxeBPcFSAjo65nQHbTA53YYn"
+              alt="Contact Person"
+            />
+            <img
+              className="my-2 mr-5 h-28 w-28 rounded-full max-w-xs "
+              src="https://colorlib.com/wp/wp-content/uploads/sites/2/creative-css3-tutorials.jpg"
+              alt="Contact Person"
+            />
+            <img
+              className="my-2 mr-5 h-28 w-28 rounded-full max-w-xs "
+              src="https://copyassignment.com/wp-content/uploads/2022/06/Django-logo-474x360-1.jpg"
+              alt="django"
+            />
+            <img
+              className="my-2 mr-5 h-28 w-28 rounded-full max-w-xs "
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+              alt="react"
+            />
+            <img
+              className="my-2 mr-5 h-28 w-28 rounded-full max-w-xs "
+              src="https://res.cloudinary.com/hevo/image/upload/v1626694700/hevo-blog/MongoDB-sm-logo-500x400-1-1.gif"
+              alt="Mongodb"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
